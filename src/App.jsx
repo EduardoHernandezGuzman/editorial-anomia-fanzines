d83089e7ce4b87fdec34c74ab278abf3aba40612
EduardoHernandezGuzman <edohdezg@gmail.com>
2024-07-16 21:15:26 +0200

    Realizados cambios en el componente Contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,41 @@
-import { Element } from 'react-scroll';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/App.css';
-import Home from './components/Home';
-import AboutFanzines from './components/AboutFanzines';
-import AboutUs from './components/AboutUs';
-import Contact from './components/Contact';
-import NavBar from './components/NavBar';
-import Store from './components/Store';
-
+import { Element } from "react-scroll";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/App.css";
+import Home from "./components/Home";
+import AboutFanzines from "./components/AboutFanzines";
+import AboutUs from "./components/AboutUs";
+import Contact from "./components/Contact";
+import NavBar from "./components/NavBar";
+import Store from "./components/Store";
 
 const App = () => {
-    return (
-        <div>
-            <NavBar/>
+  return (
+    <div>
+      <NavBar />
 
-            <Element name="home" className="section element-home" >
-                <h1>Home</h1>
-                <Home/>
-            </Element>
+      <Element name="home" className="section element-home">
+        <h1>Home</h1>
+        <Home />
+      </Element>
 
-            <Element name="aboutZine" className="section element-aboutZine" >
-                <AboutFanzines/>
-            </Element>
+      <Element name="aboutZine" className="section element-aboutZine">
+        <AboutFanzines />
+      </Element>
 
-            <Element name="aboutAnomia" className="section element-aboutAnomia" >
-                <AboutUs/>
-            </Element>
+      <Element name="aboutAnomia" className="section element-aboutAnomia">
+        <AboutUs />
+      </Element>
 
-            <Element name="store" className="section element-store" >
-                <h2>Tienda</h2>
-                <Store/>
-            </Element>
+      <Element name="store" className="section element-store">
+        <h2>Tienda</h2>
+        <Store />
+      </Element>
 
-            <Element name="contact" className="section element-contact" >
-                <h2>Contacto</h2>
-                <Contact/>
-            </Element>
-        </div>
-    );
+      <Element name="contact" className="section element-contact">
+        <Contact />
+      </Element>
+    </div>
+  );
 };
 
 export default App;
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,93 @@
-import '../styles/Contact.css'
+import { useState } from "react";
+import "../styles/Contact.css";
 
 const AboutFanzines = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
 
-    return (
-        <h4>Contacta con nosotros</h4>
-    )
-}
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const dataToSend = { ...formData };
+    console.log("Data to send:", dataToSend);
+    // Aquí puedes realizar la lógica para enviar los datos (ej. a una API)
+  };
+
+  return (
+    <div className="container">
+      <div className="row align-items-center">
+        <div className="col-6">
+          <h4>Contacta con nosotros</h4>
+
+          <form onSubmit={handleSubmit} className="mt-4">
+            <div className="form-group">
+              <label htmlFor="name">Nombre</label>
+              <input
+                type="text"
+                className="form-control m-2"
+                id="name"
+                name="name"
+                placeholder="Aleister Crowley"
+                value={formData.name}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                className="form-control m-2"
+                id="email"
+                name="email"
+                placeholder="aleister@crowley.com"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="message">Mensaje</label>
+              <textarea
+                className="form-control m-2"
+                id="message"
+                name="message"
+                rows="3"
+                placeholder="Me encantan vuestros fanzines"
+                value={formData.message}
+                onChange={handleChange}
+                required
+              ></textarea>
+            </div>
+
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
+
+        <div className="col-6">
+          <img
+            className="contact-image"
+            src="src/assets/images/003.jpg"
+            alt="aboutus"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default AboutFanzines;
