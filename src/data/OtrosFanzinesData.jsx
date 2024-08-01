66e1df2fca7de681663a657499508d9aa9fe6dfe
EduardoHernandezGuzman <edohdezg@gmail.com>
2024-08-01 20:22:05 +0200

    Creado /src/data

---
+++ src/data/OtrosFanzinesData.jsx
@@ -0,0 +1,10 @@
+const otrosFanzines = [
+  {
+    id: 1,
+    titulo: "El día más feliz de la vida",
+    image: "src/assets/images/ejemplo1.png",
+    link: "https://www.etsy.com/ejemplo1",
+  },
+];
+
+export default otrosFanzines;
