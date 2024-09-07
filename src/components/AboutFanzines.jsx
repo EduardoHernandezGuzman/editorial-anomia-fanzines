26c53f050612b9e7455e433a3853d085db25ca8e
EduardoHernandezGuzman <edohdezg@gmail.com>
2024-09-07 17:48:29 +0200

    Agregado boton y cambiado aspecto visual

--- src/components/AboutFanzines.jsx
+++ src/components/AboutFanzines.jsx
@@ -1,27 +1,38 @@
 import "../styles/AboutFanzines.css";
+import { Link } from "react-scroll";
 
 const AboutFanzines = () => {
   return (
-    <div className="container">
-      <div className="row align-items-center">
-        <div className="col-6">
-          <img
-            className="fanzine-imagen"
-            src="src/assets/images/otras/cf003.jpg"
-            alt="Fanzine"
-          />
-        </div>
-        <div className="col-6">
-          <h4>¿Qué es un fanzine?</h4>
-          <p>
-            Un fanzine es una publicación independiente y autogestionada, creada
-            por y para entusiastas de una variedad de temas.
-          </p>
-          <p>
-            A menudo producidos con técnicas de bajo costo, como la fotocopia,
-            los fanzines permiten una expresión creativa libre y sin
-            restricciones comerciales.
-          </p>
+    <div className="element-aboutZine">
+      <div className="aboutZine-container container d-flex flex-column justify-content-center align-items-center">
+        <div className="row align-items-center">
+          <div className="col-6">
+            <img
+              className="fanzine-imagen"
+              src="src/assets/images/otras/cf003.jpg"
+              alt="Fanzine"
+            />
+          </div>
+          <div className="col-6">
+            <h4>¿Qué es un fanzine?</h4>
+            <p>
+              Un fanzine es una publicación independiente y autogestionada,
+              creada por y para entusiastas de una variedad de temas.
+            </p>
+            <p>
+              A menudo producidos con técnicas de bajo costo, como la fotocopia,
+              los fanzines permiten una expresión creativa libre y sin
+              restricciones comerciales.
+            </p>
+            <Link
+              to="aboutAnomia"
+              smooth={true}
+              duration={500}
+              className="category-button aboutZine-button"
+            >
+              ¿Quienes somos?
+            </Link>
+          </div>
         </div>
       </div>
     </div>
